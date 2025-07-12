4ff5dd03d907167da2f5fe5a131794b6ba8e247f
Eloi Stree <eloistree@gmail.com>
2025-07-13 01:02:21 +0200

    Save: Add flat fitler compute shader

--- Runtime/GPT/TextureMono_ComputeFilterFlatColor.cs
+++ Runtime/GPT/TextureMono_ComputeFilterFlatColor.cs
@@ -2,29 +2,20 @@ using Eloi.TextureUtility;
 using UnityEngine;
 using UnityEngine.Events;
 
-
-
-public class TextureMono_ComputeFilterAndOrRGB : MonoBehaviour
+public class TextureMono_ComputeFilterFlatColor: MonoBehaviour
 {
     public ComputeShader m_computeShader;
     public RenderTexture m_source;
     public RenderTexture m_result;
     public UnityEvent<RenderTexture> m_onCreated;
     public UnityEvent<RenderTexture> m_onUpdated;
-   
-    public bool m_useRed;
-    public bool m_inverseRed;
-    public bool m_useGreen;
-    public bool m_inverseGreen;
-    public bool m_useBlue;
-    public bool m_inverseBlue;
 
-    public MultiColorAndOrType m_multiColorConditionType;
-    public enum MultiColorAndOrType { And, Or}
-
-    public Color32 m_colorMin;
-    public Color32 m_colorMax;
-
+    public bool m_inverseGlobal;
+    public float m_flatPercentRed = 0.1f;
+    public float m_flatPercentGreen = 0.1f;
+    public float m_flatPercentBlue = 0.1f;
+    public float m_blackPercent = 0.1f;
+    public float m_whitePercent = 0.1f;
     public void SetTexture(RenderTexture source)
     {
         m_source = source;
@@ -54,6 +45,7 @@ public class TextureMono_ComputeFilterAndOrRGB : MonoBehaviour
         if (m_useUpdate)
             ComputeTheTexture();
     }
+ 
 
     public void ComputeTheTexture()
     {
@@ -66,27 +58,18 @@ public class TextureMono_ComputeFilterAndOrRGB : MonoBehaviour
         m_computeShader.SetTexture(kernelIndex, "m_result", m_result);
         m_computeShader.SetInt("m_width", m_source.width);
         m_computeShader.SetInt("m_height", m_source.height);
-        m_computeShader.SetFloat("m_colorMinRed", m_colorMin.r / 255f);
-        m_computeShader.SetFloat("m_colorMinGreen", m_colorMin.g / 255f);
-        m_computeShader.SetFloat("m_colorMinBlue", m_colorMin.b / 255f);
-        m_computeShader.SetFloat("m_colorMaxRed", m_colorMax.r / 255f);
-        m_computeShader.SetFloat("m_colorMaxGreen", m_colorMax.g / 255f);
-        m_computeShader.SetFloat("m_colorMaxBlue", m_colorMax.b / 255f);
-
+        m_computeShader.SetInt("m_inverseGlobal", m_inverseGlobal ? 1 : 0);
+        m_computeShader.SetFloat("m_flatPercentRed", m_flatPercentRed);
+        m_computeShader.SetFloat("m_flatPercentGreen", m_flatPercentGreen);
+        m_computeShader.SetFloat("m_flatPercentBlue", m_flatPercentBlue);
+        m_computeShader.SetFloat("m_blackPercent", m_blackPercent);
+        m_computeShader.SetFloat("m_whitePercent", m_whitePercent);
+      
         int threadGroupsX = Mathf.CeilToInt(m_source.width / 8.0f);
         int threadGroupsY = Mathf.CeilToInt(m_source.height / 8.0f);
 
-        m_computeShader.SetInt("m_checkForRed", m_useRed ? 1 : 0);
-        m_computeShader.SetInt("m_checkForRedInverse", m_inverseRed ? 1 : 0);
+    
 
-        m_computeShader.SetInt("m_checkForGreen", m_useGreen ? 1 : 0);
-        m_computeShader.SetInt("m_checkForGreenInverse", m_inverseGreen ? 1 : 0);
-
-        m_computeShader.SetInt("m_checkForBlue", m_useBlue ? 1 : 0);
-        m_computeShader.SetInt("m_checkForBlueInverse", m_inverseBlue ? 1 : 0);
-        m_computeShader.SetInt("m_multiColor_or0_and1", m_multiColorConditionType==MultiColorAndOrType.Or ? 0 : 1);
-
-        
 
 
         m_computeShader.Dispatch(kernelIndex, threadGroupsX, threadGroupsY, 1);
